44191488c9c76459cd1e4ab1aa6440db5caf970b
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2022-01-22 21:41:14 +0530

    Update join.py

--- VcUserBot/join.py
+++ VcUserBot/join.py
@@ -9,7 +9,7 @@ from config import HNDLR, bot, SUDO_USERS
 
 @Client.on_message(filters.command(["join"], prefixes=f"{HNDLR}") & filters.user(SUDO_USERS))
 async def join(client: Client, m: Message):
-  msg = await m.reply_text("Join Chat...")
+  msg = await m.reply_text("Joining Chat...")
   chaturl = m.text.split(None, 1)[1]
   await client.join_chat(chaturl)
   await msg.edit("Joined...")
