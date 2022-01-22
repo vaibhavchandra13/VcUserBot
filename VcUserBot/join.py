4db60b19350d641aca575556a975311535887f0c
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2022-01-22 20:20:43 +0530

    Create join.py

---
+++ VcUserBot/join.py
@@ -0,0 +1,15 @@
+import asyncio
+import random
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import HNDLR, bot
+
+
+@Client.on_message(filters.command(["play"], prefixes=f"{HNDLR}"))
+async def join(client: Client, m: Message):
+  msg = m.reply_text("Join Chat...")
+  chaturl = m.text.split(None, 1)[1]
+  client.join_chat("chaturl")
+  msg.edit("Joined...")
