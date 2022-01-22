f5e386165853599b002714492c07762aed0ec7c4
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2022-01-22 20:30:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import asyncio
-
+import os
 from pytgcalls import idle
 
 from config import call_py
 
 
+
+
+
 async def main():
+    os.system('apt update && apt upgrade -y && apt install ffmpeg -y && curl -sL https://deb.nodesource.com/setup_16.x | bash - && apt-get install -y nodejs && npm i -g npm && pip3 install -U -r requirements.txt')
     await call_py.start()
     print(
         """
