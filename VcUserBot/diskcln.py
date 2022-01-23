d1ecf4faa098644f4911c8a67caa9ce238a1ff2e
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2022-01-23 18:39:43 +0530

    Create diskcln.py

---
+++ VcUserBot/diskcln.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, emoji, filters
+from config import HNDLR, SUDO_USERS
+from pyrogram.types import Message
+import os
+
+downloads = os.path.realpath("downloads")
+raw = os.path.realpath("raw_files")
+
+@Client.on_message(filters.command(["cleardl"], prefixes=f"{HNDLR}") & filters.user(SUDO_USERS))
+async def cleardisk(client, m: Message):
+    ls_dir = os.listdir(downloads)
+    if ls_dir:
+        for file in os.listdir(downloads):
+            os.remove(os.path.join(downloads, file))
+        await message.reply_text("**Removed All Downloads**")
+    else:
+        await message.reply_text("**NO FILES FOUND**")
+        
+        
+@Client.on_message(filters.command(["clearjpg"], prefixes=f"{HNDLR}") & filters.user(SUDO_USERS))
+async def clearjpg(client, m: Message):
+    pth = os.path.realpath(".")
+    ls_dir = os.listdir(pth)
+    if ls_dir:
+        for dta in os.listdir(pth):
+            os.system("rm -rf *.jpg")
+        await message.reply_text("**Removed All JPG Files**")
+    else:
+        await message.reply_text("**NO FILES FOUND**")
