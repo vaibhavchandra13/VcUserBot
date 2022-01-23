dd7b0a7b191fb426be0c64b0034f9967464201b5
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2022-01-23 18:35:05 +0530

    Update sudowlcm.py

--- VcUserBot/sudowlcm.py
+++ VcUserBot/sudowlcm.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, emoji, filters
 from config import HNDLR, SUDO_USERS
-
+from pyrogram.types import Message
 
 @Client.on_message(filters.new_chat_members & filters.user(SUDO_USERS))
-async def sudowlcm(client, message: Message):
+async def sudowlcm(client, m: Message):
     m.reply_text("**Hey Here Joined My SUDO User**")
