40e2801d7d054f2695dcdc9c560d37f2e02d0fb8
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2022-01-23 17:22:49 +0530

    Update user_id.py

--- VcUserBot/user_id.py
+++ VcUserBot/user_id.py
@@ -5,10 +5,13 @@ from config import HNDLR, SUDO_USERS
 from VcUserBot.helpers.get_file_id import get_file_id
 
 
-@Client.on_message(filters.command(["id"], prefixes=f"{HNDLR}") & filters.user(SUDO_USERS))
+@Client.on_message(filters.command(["id"], prefixes=f"{HNDLR}"))
 async def showid(_, message: Message):
     chat_type = message.chat.type
-
+    chat_id = m.chat.id
+    if not await CHATS:
+        await message.reply_text(f"**This is A Music Bot Made Only for Private Use in Some Specific Chats**")
+        return await Client.leave_chat(chat_id) 
     if chat_type == "private":
         user_id = message.chat.id
         await message.reply_text(f"<code>{user_id}</code>")
