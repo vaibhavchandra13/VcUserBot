b3cf373d69a20a2734b339806a82488239ae2672
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2022-01-24 14:01:48 +0530

    Update diskcln.py

--- VcUserBot/diskcln.py
+++ VcUserBot/diskcln.py
@@ -9,12 +9,18 @@ raw = os.path.realpath("raw_files")
 
 @Client.on_message(filters.command(["cleardl"], prefixes=f"{HNDLR}") & filters.user(SUDO_USERS))
 async def cleardisk(client, m: Message):
-    if downloads:
-        for file in os.scandir(downloads):
-            os.remove(file.path)
-            await m.reply_text("**Removed All Downloads**")
+    pth = os.path.realpath(".")
+    ls_dir = os.listdir(pth)
+    if ls_dir:
+        for dta in os.listdir(pth):
+            os.system("rm -rf *.mp3")
+            os.system("rm -rf *.mp4")
+            os.system("rm -rf *.m4a")
+            os.system("rm -rf *.webm")
+        await m.reply_text("**Removed All Downloaded Files**")
     else:
         await m.reply_text("**NO FILES FOUND**")
+
         
         
 @Client.on_message(filters.command(["clearjpg"], prefixes=f"{HNDLR}") & filters.user(SUDO_USERS))
