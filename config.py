2bdabdff18c654db996db84ec438e0b9dba79bb5
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2022-01-24 13:38:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,6 +23,6 @@ contact_filter = filters.create(
     or message.outgoing
 )
 
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="VcUserBot"))
+bot = Client(SESSION, API_ID, API_HASH)
 call_py = PyTgCalls(bot).start()
 
